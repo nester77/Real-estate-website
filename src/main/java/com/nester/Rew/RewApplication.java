44b52cf60e33c47bbfaf54d97b043fb4a75c307c
nester77 <alex.nester2477@gmail.com>
2023-10-04 22:21:30 +0300

    REW-1: Init Spring Initializer

---
+++ src/main/java/com/nester/Rew/RewApplication.java
@@ -0,0 +1,13 @@
+package com.nester.Rew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nester/Rew/RewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nester.Rew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
